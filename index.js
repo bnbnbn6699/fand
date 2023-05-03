9fe837a01978d730be9447c23bd04c41f3d88550
bnbnbn6699 <128575370+bnbnbn6699@users.noreply.github.com>
2023-05-03 17:16:26 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Contact from './Components/Contact';
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var jwt = require('jsonwebtoken');
+const secret = 'Fullstack-Login-2021'
+const saltRounds = 10
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/Contact" element={<Contact/>}/>
-    </Routes>
-   </BrowserRouter>
-  
-);
+app.use(cors())
+
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'mydb'
+  });
+
+app.post('/contact', jsonParser, function (req, res) {
+connection.execute(
+    'INSERT INTO user (fname, email, text ) VALUES (?, ?, ?)',
+    [req.body.fname, req.body.email, req.body.text],
+    function(err, results, fields) {
+        if (err) {
+            res.json({status:'eror', message: err})
+            return
+        }
+      res.json({status: 'ok'})
+    }
+  );
+})
+
+
+    app.listen(3333, function () {
+  console.log('Example app listening on port 3333')
+})
+    
